94adbb5ac64491e9bb21dd8d05a85a707f49f366
halmk <elle2206@i.softbank.jp>
2022-08-11 15:25:05 +0900

    Create ClipDialog component

---
+++ frontend/components/molecules/ClipDialog.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-dialog
+    :value="showDialog"
+    :width="bkPoint.dialogWidth"
+    hide-overlay
+    @click:outside="close"
+    @keydown="close"
+  >
+    <iframe
+      :src="embedClipURL"
+      :height="bkPoint.dialogHeight"
+      :width="bkPoint.dialogWidth"
+      allowfullscreen
+    >
+    </iframe>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ClipDateRangePicker',
+  props: {
+    embedClipURL: String,
+    showDialog: Boolean,
+  },
+  data: () => ({}),
+  computed: {
+    bkPoint() {
+      const bkPt = this.$vuetify.breakpoint
+      const point = {
+        dialogHeight: 9 * 60,
+        dialogWidth: 16 * 60,
+      }
+      switch (bkPt.name) {
+        case 'md':
+          point.dialogHeight = 9 * 50
+          point.dialogWidth = 16 * 50
+          break
+        case 'sm':
+          point.dialogHeight = 9 * 40
+          point.dialogWidth = 16 * 40
+          break
+        case 'xs':
+          point.dialogHeight = 9 * 20
+          point.dialogWidth = 16 * 20
+          break
+        default:
+          break
+      }
+      return point
+    },
+  },
+  mounted() {},
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
