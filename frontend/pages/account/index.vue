a4807e4179c69d98f8427d5c6158b6b2303d0f67
halmk <elle2206@i.softbank.jp>
2022-08-25 08:28:37 +0900

    #29 Create account page

---
+++ frontend/pages/account/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col class="" cols="12" sm="8" md="10" lg="12">
+        <NuxtLink to="/account/login">Login</NuxtLink>
+        <NuxtLink to="/account/logout">Logout</NuxtLink>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccountIndexPage',
+  middleware: 'auth',
+  head() {
+    return {
+      title: 'Home',
+    }
+  },
+}
+</script>
