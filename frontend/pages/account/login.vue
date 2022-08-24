0bc660c2e235b29c64d88c9a9b7c7abc0ec715f5
halmk <elle2206@i.softbank.jp>
2022-08-25 08:28:50 +0900

    #29 Create login page

---
+++ frontend/pages/account/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col class="mx-auto" sm="6" md="6" lg="4">
+        <v-card>
+          <v-card-title class="justify-center text-h4">
+            Log in to Cliphype
+          </v-card-title>
+          <v-card-text class="text-center">
+            <TwitchSocialLogin />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  middleware: 'auth',
+  head() {
+    return {
+      title: 'Login',
+    }
+  },
+}
+</script>
