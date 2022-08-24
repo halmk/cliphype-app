269f4c9103283f5adc73057071b88b10c59f2885
halmk <elle2206@i.softbank.jp>
2022-08-25 08:25:03 +0900

    #29 Create LogoutButton.vue

---
+++ frontend/components/molecules/LogoutButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn color="primary" class="ma-2 white--text" @click.stop="logout">
+    <v-icon left> mdi-logout </v-icon>
+    Logout
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'LogoutButton',
+  methods: {
+    logout() {
+      window.location.href = `${this.$config.apiURL}/accounts/logout`
+    },
+  },
+}
+</script>
