e96df9ccf60120d544429063e74b7847babae87a
halmk <elle2206@i.softbank.jp>
2022-08-29 09:47:35 +0900

    #42 Create callback page

---
+++ frontend/pages/account/twitch/login/callback.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>attempting to login...</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'TwitchLoginCallbackPage',
+  head() {
+    return {
+      title: 'Login',
+    }
+  },
+  async mounted() {
+    console.log(this.$route.query)
+    const params = this.$route.query
+    try {
+      const response = await axios.get(
+        `${this.$config.apiURL}/accounts/twitch/login/callback`,
+        {
+          params,
+          withCredentials: true,
+        }
+      )
+      const jwt = response.data.token
+      const expires = response.data.expires
+      this.$cookies.set('jwt', jwt, {
+        path: '/',
+        expires: new Date(expires),
+        httpOnly: false,
+        secure: true,
+      })
+      this.$store.commit('auth/setSession', this.$cookies.get('jwt'))
+      this.$router.push('/account')
+    } catch (error) {
+      console.log(error)
+    }
+  },
+}
+</script>
