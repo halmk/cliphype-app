5fa985999e6132f3445def3165d4bd25e6ae424d
halmk <elle2206@i.softbank.jp>
2022-09-08 15:08:59 +0900

    #69 Create: chatbot plugin

---
+++ frontend/plugins/chatbot.js
@@ -0,0 +1,51 @@
+import tmi from 'tmi.js'
+
+const ChatBot = {
+  client: null,
+  username: '',
+  password: '',
+  channel: '',
+  messages: [],
+
+  // Define configuration options
+  getOpts() {
+    return {
+      identity: {
+        username: ChatBot.username,
+        password: ChatBot.password,
+      },
+      channels: [ChatBot.channel],
+    }
+  },
+
+  getMessages() {
+    return ChatBot.messages
+  },
+
+  initOpts(username, password, channel) {
+    ChatBot.username = username
+    ChatBot.password = password
+    ChatBot.channel = channel
+  },
+
+  newClient(opts) {
+    ChatBot.client = new tmi.Client(opts)
+  },
+
+  onEvent(event, func) {
+    ChatBot.client.on(event, func)
+  },
+
+  pushMessage(message) {
+    ChatBot.messages.push(message)
+  },
+
+  connect() {
+    // Connect to Twitch:
+    ChatBot.client.connect()
+  },
+}
+
+export default ({ app }, inject) => {
+  inject('chatbot', ChatBot)
+}
