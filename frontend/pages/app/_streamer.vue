83c024ea94f1faaf178cdf8e8dd8ee155e00a92c
halmk <elle2206@i.softbank.jp>
2022-09-13 19:37:59 +0900

    Fix: render nuxt-child component only equal to the tab

--- frontend/pages/app/_streamer.vue
+++ frontend/pages/app/_streamer.vue
@@ -7,12 +7,15 @@
             {{ streamer }}
           </h1>
           <v-tabs v-model="activeTab">
-            <v-tab v-for="tab of tabs" :key="tab.id" :to="tab.route" exact nuxt>
+            <v-tab v-for="tab of tabs" :key="tab.id" :to="tab.route" exact>
               {{ tab.name }}
             </v-tab>
 
             <v-tab-item v-for="tab of tabs" :key="tab.id" :value="tab.route">
-              <nuxt-child></nuxt-child>
+              <nuxt-child
+                v-if="activeTab === tab.route"
+                :key="$route.fullPath"
+              ></nuxt-child>
             </v-tab-item>
           </v-tabs>
         </v-container>
@@ -26,7 +29,7 @@ export default {
   name: 'StreamerPage',
   asyncData({ params }) {
     return {
-      activeTab: `/app/${params.streamer}/clip`,
+      activeTab: '',
       tabs: [
         { id: 1, name: 'Clip', route: `/app/${params.streamer}/clip` },
         { id: 2, name: 'Playlist', route: `/app/${params.streamer}/playlist` },
