56889ac253a1cf0dfeff5481b2d71d1808fcbfcc
halmk <elle2206@i.softbank.jp>
2022-09-16 18:35:11 +0900

    #79 Add: set token and dispatch action related to user info

--- frontend/middleware/user.js
+++ frontend/middleware/user.js
@@ -1,10 +1,12 @@
 export default async function ({ store, app }) {
   store.commit('auth/setSession', app.$cookies.get('jwt'))
+  store.commit('user/setToken', app.$cookies.get('jwt'))
   const isLogined = store.getters['auth/isLogined']
   const isExistUser = store.getters['user/isExistUser']
   if (isLogined) {
     if (!isExistUser) {
       await store.dispatch('user/getUser')
+      await store.dispatch('user/getTwitchUser')
     }
     const isFollows = store.getters['user/isFollows']
     if (!isFollows) {
