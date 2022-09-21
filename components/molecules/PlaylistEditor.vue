1697baf27d32a8c24ebebff067b8f0f649695434
halmk <sakurai.apple1@gmail.com>
2022-09-21 20:43:41 +0000

    #3 Create: PlaylistEditor enable edit playlist

---
+++ components/molecules/PlaylistEditor.vue
@@ -0,0 +1,50 @@
+<template>
+  <PlaylistDialog
+    :clips="clips"
+    :show="show"
+    :loading="loading"
+    editable
+    @clickClose="closePlaylist"
+    @clickPublish="clickPublish"
+    @clickSort="clickSort"
+    @changeTitle="changeTitle"
+    @clickPrev="swapPrevClip"
+    @clickNext="swapNextClip"
+    @clickRemove="removeClip"
+  ></PlaylistDialog>
+</template>
+
+<script>
+export default {
+  name: 'ClipPlaylist',
+  props: {
+    clips: Array,
+    show: Boolean,
+    loading: Boolean,
+  },
+  data: () => ({}),
+  methods: {
+    clickSort() {
+      this.$emit('clickSort')
+    },
+    clickPublish() {
+      this.$emit('clickPublish')
+    },
+    changeTitle(title) {
+      this.$emit('changeTitle', title)
+    },
+    closePlaylist() {
+      this.$emit('clickClose')
+    },
+    removeClip(index) {
+      this.$emit('clickRemove', index)
+    },
+    swapPrevClip(index) {
+      this.$emit('clickPrev', index)
+    },
+    swapNextClip(index) {
+      this.$emit('clickNext', index)
+    },
+  },
+}
+</script>
